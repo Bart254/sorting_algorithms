e2c11f2041e92fb7f0a2f68615b782302099e83f
Bart254 <omondibartlett3@gmail.com>
2023-09-30 23:12:42 +0300

    modify func

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,47 +1,73 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
- * quick_sort - uses Lomuto Technique to sort an array
- * @array: array of integers to be sorted
- * @size: size of the array
- *
- * The function sorts the integer array
- * and prints the array every time indices are swapped
+ * swap_e - swaps two int
+ * @a: int a
+ * @b: int b
+ */
+void swap_e(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * lom_part - uses lomunto method
+ * @array: the array of int
+ * @size: size of array
+ * @low: low
+ * @high: highest index
+ * Return: next index
+ */
+int lom_part(int *array, size_t size, int low, int high)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			swap_e(&array[i], &array[j]);
+		}
+	}
+	swap_e(&array[i + 1], &array[high]);
+	print_array(array, size);
+	return (i + 1);
+}
+
+/**
+ * help - complementary solver
+ * @array: array of int
+ * @size: size of array
+ * @low: lowest idx
+ * @high: highest idx
+ */
+void help(int *array, size_t size, int low, int high)
+{
+	if (low < high)
+	{
+		int pivot_index = lom_part(array, size, low, high);
+
+		help(array, size, low, pivot_index - 1);
+		help(array, size, pivot_index + 1, high);
+	}
+}
+
+/**
+ * quick_sort - main function
+ * @array: arr of int
+ * @size: size of array
  */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL)
-		return;
-	sort(array, 0, size);
-}
-
-/**
- * sort - sorts the array
- * @array: subarray to be sorted
- * @low: lowest index to sort
- * @high: highest index to sort
- */
-void sort(int *array, int low, int high)
-{
-	int a = low -1, b, pivot = array[high], temp;
-
-	if (low >= high)
-		return;
-	for (b = low; b < high; b++)
+	if (array == NULL || size < 2)
 	{
-		if (array[b] <= pivot)
-		{
-			a++;
-			temp = array[b];
-			array[b] = array[a];
-			array[a] = temp;
-			print_array(array, high + 1);
-		}
+		return;
 	}
-	temp = array[high];
-	array[high] = array[a + 1];
-	array[a + 1] = temp;
-	sort(array, low, a - 1);
-	sort(array, a + 1, high);
+	help(array, size, 0, size - 1);
 }
-
